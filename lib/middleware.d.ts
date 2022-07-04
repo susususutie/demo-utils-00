5d2cd728a0da54103c9124dfbe85e021ede37f01
liuyi <liuyi@mchz.com.cn>
2022-07-04 12:09:30 +0800

    update gitignore

--- lib/middleware.d.ts
+++
@@ -1,4 +0,0 @@
-export interface IMiddleware<Payload = any, Result = any> {
-    (payload: Payload, next: (payload?: Payload) => Result): Result;
-}
-export declare const applyMiddleware: (payload: any, fns?: IMiddleware[]) => Promise<unknown>;
